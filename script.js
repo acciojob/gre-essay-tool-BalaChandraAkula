e93335bf940eba12fdfad283ee1b45aa4f132a9e
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-03-02 14:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const evaluate = document.querySelector("#evaluatedText");
+const wordCount = document.querySelector("#wordCount");
+
+evaluate.addEventListener("input", () => {
+  let txt = evaluate.value.trim();
+  wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
+});
